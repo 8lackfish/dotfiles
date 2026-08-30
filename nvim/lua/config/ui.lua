87089b014009ff85cc4f8c90599fd03d66387fd5
8lackfish <148613625+8lackfish@users.noreply.github.com>
2026-08-31 01:36:29 +0700

    refactor(nvim): switch to bufline.nvim

--- nvim/lua/config/ui.lua
+++
@@ -1,5 +0,0 @@
--- Tabline
-require'ui.tabline'.setup_bufline({
-  hl = 'lualine_b_normal',
-  hl_sel = 'lualine_a_insert'
-})
---
+++ nvim/lua/plugins/bufline.lua
@@ -0,0 +1,9 @@
+return {
+  "8lackfish/bufline.nvim",
+  opts = {
+    highlights = {
+      background = 'lualine_b_normal',
+      focused = 'lualine_a_insert'
+    }
+  }
+}
